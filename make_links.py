92566ea818b8f375123f1f379b9b11d0b5733076
Mike Wilkerson <mike.wilkerson@willowtreeapps.com>
2013-07-02 16:27:54 -0400

    Adding numbers to make it Python <2.7 friendly.

--- make_links.py
+++ make_links.py
@@ -8,15 +8,15 @@ repo_root = os.path.realpath(os.path.dirname(__file__))
 backup_files_dir = os.path.join(home_dir, 'toolbag_file_backup')
 
 print
-print "home_dir is {}".format(home_dir)
-print "repo_root is {}".format(repo_root)
-print "backup_files_dir is {}".format(backup_files_dir)
+print "home_dir is {0}".format(home_dir)
+print "repo_root is {0}".format(repo_root)
+print "backup_files_dir is {0}".format(backup_files_dir)
 print
 
 # Create a directory for backing up existing non-symlinked files.
 if not os.path.exists(backup_files_dir):
     print
-    print "Making a directory to store backed-up files in: {}".format(
+    print "Making a directory to store backed-up files in: {0}".format(
         backup_files_dir
     )
     print
@@ -38,17 +38,17 @@ for the_file in managed_files:
     actual_file_path = os.path.join(home_dir, the_file)
 
     if os.path.islink(actual_file_path):
-        print "{} is already symlinked! Skipping...".format(the_file)
+        print "{0} is already symlinked! Skipping...".format(the_file)
     else:
         move_path = os.path.join(backup_files_dir, the_file)
         repo_path = os.path.join(repo_root, the_file)
-        print "Moving from {} to {}".format(actual_file_path, move_path)
+        print "Moving from {0} to {1}".format(actual_file_path, move_path)
 
         # Move the file to the backup directory.
         os.rename(actual_file_path, move_path)
 
         # Then create a symlink back where we just moved it from.
-        print "Creating a symlink at {} to {}".format(
+        print "Creating a symlink at {0} to {1}".format(
             actual_file_path,
             repo_path
         )
