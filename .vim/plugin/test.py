7539a1919e84b1583d57c629ee7927b54be11a72
Mike Wilkerson <mike.wilkerson@willowtreeapps.com>
2013-11-23 00:55:38 -0500

    nothing here

--- .vim/plugin/test.py
+++ .vim/plugin/test.py
@@ -1,13 +1,23 @@
 import os
 import re
 
-filename = "plugin_test.vim"
+filename = "mike_test.py"
 cwd = os.path.realpath(".")
+fullpath = os.path.realpath(
+    os.path.join(
+        cwd,
+        "..",
+        "..",
+        "..",
+        filename,
+    )
+)
 
-with open(filename) as f:
+with open(fullpath) as f:
     lines = [line.strip() for line in f.readlines()]
     for linenum, line in enumerate(lines):
-        if re.compile(r"^(class|def|func|\(defun)").search(line):
+        # if re.compile(r"^(class|def|func|\(defun)").search(line):
+        if re.compile(r"^(class |def |func\|\(defun )").search(line):
             print "{} is on line {} of {}".format(
                 line.split(" ")[1],
                 linenum+1,
