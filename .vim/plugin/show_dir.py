ca3cad665116f53da1b148e9af362a211ab93f99
Mike Wilkerson <mike.wilkerson@willowtreeapps.com>
2014-02-21 15:15:09 -0500

    WIP

--- .vim/plugin/show_dir.py
+++ .vim/plugin/show_dir.py
@@ -1,57 +1,51 @@
-import sys
+import re
+
+lines = [
+    'import datetime',
+    'from lxml.html import parse',
+    'from lxml.html import urljoin, tostring',
+    'from lxml.html import ',
+    'from datetime import datetime',
+    'import os, sys',
+]
+regex_string = '(from ){0,1}([\w.]+){0,1} *import *([\w., ]+)'
+regex = re.compile(regex_string)
 
 
-def print_results(the_string):
-    print "-" * 80
-    print "Final results"
-    print "-" * 80
-    print the_string
+def get_imports_list(import_string):
+    return [i.strip() for i in import_string.split(',') if i.strip()]
 
 
-def strip_import_statements(the_string):
-    return the_string.replace(
-        "from", ""
-    ).replace(
-        "import", ""
-    ).strip()
+def do_import(import_string):
+    exec("import {}".format(the_import))
 
 
-def get_dir_from_string(the_string):
-    try:
-        exec("import {}".format(the_string))
-        return dir(sys.modules[the_string])
-    except ImportError:
-        print "ImportError in get_dir_from_string!"
-        pass
+for line in lines:
+    _, from_module, import_modules = regex.search(line).groups()
 
-    try:
-        return dir(globals()['parse'])
-    except KeyError:
-        print "KeyError in get_dir_from_string!"
-        pass
+    if isinstance(from_module, str):
+        from_module = from_module.strip()
 
-    return None
+    if isinstance(import_modules, str):
+        import_modules = import_modules.strip()
 
-
-if __name__ == "__main__":
-    if len(sys.argv) == 2:
-        import_line = sys.argv[1]
-        print "import_line was {}".format(import_line)
-
-        if "from" or "import" in import_line:
-            # The user has typed "import".
-
-            from_module, to_import = import_line.split('import', 1)
-            from_module = strip_import_statements(from_module)
-            print "from_module was {}".format(from_module)
-            to_import = strip_import_statements(to_import)
-            print "to_import was {}".format(to_import)
-
-            if to_import:
-                print_results(get_dir_from_string(to_import))
-                sys.exit(0)
-            else:
-                print_results(get_dir_from_string(from_module))
-                sys.exit(0)
+    if from_module and import_modules:
+        # Fully typed import from another module.
+        for the_import in get_imports_list(import_modules):
+            print "RULE 1: from {} import {}".format(
+                from_module,
+                the_import,
+            )
+    elif from_module:
+        # Partially-typed import line.
+        print "RULE 2: import {}".format(
+            from_module,
+        )
+    elif import_modules:
+        # Straight-forward import.
+        for the_import in get_imports_list(import_modules):
+            print "RULE 3: import {}".format(
+                the_import,
+            )
     else:
-        print "usage: {} <name of python module>".format(sys.argv[0])
+        print "No pattern match found for \"{}\"".format(line)
