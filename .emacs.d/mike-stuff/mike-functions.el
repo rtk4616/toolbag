5db6bd86b49f2ae9f8d085880acac6d58a0b3739
Mike Wilkerson <mike.wilkerson@willowtreeapps.com>
2014-12-22 18:45:20 -0500

    Just some text changes

--- .emacs.d/mike-stuff/mike-functions.el
+++ .emacs.d/mike-stuff/mike-functions.el
@@ -313,10 +313,10 @@ Specifying REVERSE as t will result in traversing the file backward."
 
 
 (defun MikeGrepInFiles (search_string file_extension)
-  (interactive "sSearch for: \nsIn files ending in: ")
+  (interactive "sFind string: \nsLook in files ending with: ")
   (rgrep search_string (concat "*" file_extension) "./"))
 
 
 (defun MikeGrepForFiles (search_string)
-  (interactive "sSearch for file: ")
+  (interactive "sFind files by glob: ")
   (find-name-dired "." (concat "*" search_string "*")))
