7fe913270cc576a7c71eae8902f13a91c0413401
Mike Wilkerson <mike.wilkerson@willowtreeapps.com>
2015-01-07 02:01:16 -0500

    Adding helm-ag

---
+++ .emacs.d/elpa/helm-ag-20150103.735/helm-ag-autoloads.el
@@ -0,0 +1,50 @@
+;;; helm-ag-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+
+;;;### (autoloads (helm-do-ag helm-ag helm-ag-this-file helm-ag-clear-stack
+;;;;;;  helm-ag-pop-stack) "helm-ag" "Users/mike/.emacs.d/elpa/helm-ag-20150103.735/helm-ag.el"
+;;;;;;  (21676 55454 0 0))
+;;; Generated autoloads from Users/mike/.emacs.d/elpa/helm-ag-20150103.735/helm-ag.el
+
+(autoload 'helm-ag-pop-stack "helm-ag" "\
+
+
+\(fn)" t nil)
+
+(autoload 'helm-ag-clear-stack "helm-ag" "\
+
+
+\(fn)" t nil)
+
+(autoload 'helm-ag-this-file "helm-ag" "\
+
+
+\(fn)" t nil)
+
+(autoload 'helm-ag "helm-ag" "\
+
+
+\(fn &optional BASEDIR)" t nil)
+
+(autoload 'helm-do-ag "helm-ag" "\
+
+
+\(fn &optional BASEDIR)" t nil)
+
+;;;***
+
+;;;### (autoloads nil nil ("Users/mike/.emacs.d/elpa/helm-ag-20150103.735/helm-ag-pkg.el")
+;;;;;;  (21676 55454 633082 0))
+
+;;;***
+
+(provide 'helm-ag-autoloads)
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; helm-ag-autoloads.el ends here
