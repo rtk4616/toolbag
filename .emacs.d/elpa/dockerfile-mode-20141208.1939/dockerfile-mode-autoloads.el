9b8a13b1778808b3909e5286338eb96d71285cdd
Mike Wilkerson <wilkystyle@gmail.com>
2015-02-20 16:57:10 +0000

    Adding dockerfile-mode for Emacs

---
+++ .emacs.d/elpa/dockerfile-mode-20141208.1939/dockerfile-mode-autoloads.el
@@ -0,0 +1,32 @@
+;;; dockerfile-mode-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+
+;;;### (autoloads (dockerfile-mode) "dockerfile-mode" "../../.emacs.d/elpa/dockerfile-mode-20141208.1939/dockerfile-mode.el"
+;;;;;;  (21735 26261 282106 438000))
+;;; Generated autoloads from ../../.emacs.d/elpa/dockerfile-mode-20141208.1939/dockerfile-mode.el
+
+(autoload 'dockerfile-mode "dockerfile-mode" "\
+A major mode to edit Dockerfiles.
+\\{dockerfile-mode-map}
+
+\(fn)" t nil)
+
+(add-to-list 'auto-mode-alist '("Dockerfile\\'" . dockerfile-mode))
+
+;;;***
+
+;;;### (autoloads nil nil ("../../.emacs.d/elpa/dockerfile-mode-20141208.1939/dockerfile-mode-pkg.el")
+;;;;;;  (21735 26261 292587 726000))
+
+;;;***
+
+(provide 'dockerfile-mode-autoloads)
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; dockerfile-mode-autoloads.el ends here
