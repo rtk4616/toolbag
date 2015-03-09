5d18740a2a5ef2abcac921daf0b7064046e0b2eb
Mike Wilkerson <mikewilkerson@unknown-DHCP-client-125-0-2-10.resonatedigital.net>
2015-03-09 17:17:00 -0400

    Updating emacs packages.

---
+++ .emacs.d/elpa/company-go-20150109.2051/company-go-autoloads.el
@@ -0,0 +1,22 @@
+;;; company-go-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
+
+;;;### (autoloads nil "company-go" "../.emacs.d/elpa/company-go-20150109.2051/company-go.el"
+;;;;;;  (21758 3138 0 0))
+;;; Generated autoloads from ../.emacs.d/elpa/company-go-20150109.2051/company-go.el
+
+(autoload 'company-go "company-go" "\
+
+
+\(fn COMMAND &optional ARG &rest IGNORED)" nil nil)
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; company-go-autoloads.el ends here
