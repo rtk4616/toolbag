9bb39a5085d9772243519fbc01838a39508dc9b3
Mike Wilkerson <mike@cloudistics.com>
2015-09-17 16:25:02 -0400

    Updating Emacs

--- .emacs.d/elpa/magit-gitflow-20150702.20/magit-gitflow-autoloads.el
+++
@@ -1,16 +0,0 @@
-;;; magit-gitflow-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
-
-;;;### (autoloads nil nil ("magit-gitflow.el") (21914 52331 839973
-;;;;;;  0))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; magit-gitflow-autoloads.el ends here
