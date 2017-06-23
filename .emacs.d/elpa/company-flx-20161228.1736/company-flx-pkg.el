96ec89e5ffe2a7001d56db2ce886368445f9ce11
Mike Wilkerson <wilkystyle@gmail.com>
2017-06-23 10:10:06 -0400

    Removing company-flx, due to performance issues
    Completion with the company-capf was particularly horrendous. In a shell
    script, typing input and the pop-up dialog would be delayed by about a half
    second.

--- .emacs.d/elpa/company-flx-20161228.1736/company-flx-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "company-flx" "20161228.1736" "flx based fuzzy matching for company" '((emacs "24") (company "0.8.12") (flx "0.5")) :commit "0c9fddf7c11c918ed42bd6a5108f8969b37704d5" :url "https://github.com/PythonNut/company-flx" :keywords '("convenience" "company" "fuzzy" "flx"))
