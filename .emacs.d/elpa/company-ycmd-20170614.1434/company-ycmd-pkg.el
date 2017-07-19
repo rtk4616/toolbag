2f7cfac78e2f035eed1e4e6150fa72dc89cc2cd6
Mike Wilkerson <wilkystyle@gmail.com>
2017-07-19 17:40:10 -0400

    Updating Emacs packages

--- .emacs.d/elpa/company-ycmd-20170614.1434/company-ycmd-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "company-ycmd" "20170614.1434" "company-mode backend for ycmd" '((ycmd "1.2") (company "0.9.3") (deferred "0.5.1") (s "1.11.0") (dash "2.13.0") (let-alist "1.0.5") (f "0.19.0")) :commit "d042a673b4d717c3ca9d641f120bfe16c994c740" :url "https://github.com/abingham/emacs-ycmd")
