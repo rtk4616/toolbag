bfef418c0ff6e9dee7c107bbba634672c7ecd131
Mike Wilkerson <wilkystyle@gmail.com>
2017-08-07 09:37:53 -0400

    Updating Emacs packages

--- .emacs.d/elpa/monokai-theme-20170719.146/monokai-theme-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "monokai-theme" "20170719.146" "A fruity color theme for Emacs." 'nil :commit "3939cb7f7f060ccf25d21726fe318ada2c3be7d3" :url "http://github.com/oneKelvinSmith/monokai-emacs")
