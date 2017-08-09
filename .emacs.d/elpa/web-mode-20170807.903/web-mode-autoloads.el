38e3c4214efbd9abc72174a139178afcfe440429
Mike Wilkerson <wilkystyle@gmail.com>
2017-08-09 14:49:02 -0400

    Updating Emacs packages

--- .emacs.d/elpa/web-mode-20170807.903/web-mode-autoloads.el
+++ .emacs.d/elpa/web-mode-20170807.903/web-mode-autoloads.el
@@ -3,7 +3,7 @@
 ;;; Code:
 (add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
 
-;;;### (autoloads nil "web-mode" "web-mode.el" (22895 54857 0 0))
+;;;### (autoloads nil "web-mode" "web-mode.el" (22923 5865 0 0))
 ;;; Generated autoloads from web-mode.el
 
 (autoload 'web-mode "web-mode" "\
