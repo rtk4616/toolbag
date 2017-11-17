b0186237286a44812ee4c024ce638f8ca27a7ed6
Mike Wilkerson <wilkystyle@gmail.com>
2017-11-17 13:24:24 -0500

    Don't include packages directory, now that we're using use-package

--- .emacs.d/elpa/dracula-theme-20170412.845/dracula-theme-autoloads.el
+++
@@ -1,29 +0,0 @@
-;;; dracula-theme-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-
-(add-to-list 'load-path (directory-file-name
-                         (or (file-name-directory #$) (car load-path))))
-
-
-;;;### (autoloads nil "dracula-theme" "../../../../.emacs.d/elpa/dracula-theme-20170412.845/dracula-theme.el"
-;;;;;;  "ab4fc3af576c0256b9f61fc0d6286d53")
-;;; Generated autoloads from ../../../../.emacs.d/elpa/dracula-theme-20170412.845/dracula-theme.el
-
-(when load-file-name (add-to-list 'custom-theme-load-path (file-name-as-directory (file-name-directory load-file-name))))
-
-;;;***
-
-;;;### (autoloads nil nil ("../../../../.emacs.d/elpa/dracula-theme-20170412.845/dracula-theme-autoloads.el"
-;;;;;;  "../../../../.emacs.d/elpa/dracula-theme-20170412.845/dracula-theme.el")
-;;;;;;  (0 0 0 0))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; coding: utf-8
-;; End:
-;;; dracula-theme-autoloads.el ends here
